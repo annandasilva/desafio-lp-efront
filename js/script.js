3289b1cdbfdac2b57a08f3689e221a634fa0995a
Annanda <annandacopadosilva@gmail.com>
2023-01-15 18:57:41 -0300

    responsivo e animações

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//window.sr = ScrollReveal({ reset: true });
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '50px',
+  duration: 2000,
+});
+
+sr.reveal('.delay200', { delay: 200 });
+sr.reveal('.delay400', { delay: 400 });
